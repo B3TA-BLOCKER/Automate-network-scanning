435df1de6c95efafc07f33fb5c9cd3705d24edc6
root <root@kali>
2024-08-18 19:01:53 +0500

    running netDiscover on the local subnet

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ import subprocess
 import psutil  # provides an easy way to interact with system and process utilities
 
 # The function automates running shell commands and retrieving their output as a cleaned-up string.
-def bash(command):
-    return subprocess.check_output(command, shell=True).decode('utf-8').strip()
+def bash(command) ->str :
+
+    try:
+        return subprocess.check_output(command, shell=True).decode('utf-8').strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error: {e.output.decode('utf-8')}")
+        raise
 
 
 def network_interface_info() ->str :
@@ -26,38 +31,50 @@ def network_interface_info() ->str :
         if stats.isup: # if the interfface is up and running it will be appended in the list "list_of_interfaces_running"
             list_of_interfaces_running.append(interface)
     
-
     iterations = len(list_of_interfaces_running)
     
     while True:
             # Give choice to the user to chose the desire running network interface
-            print("\n\n\t\t\t\tChoose One of the foillowing Network Interface :")
+            print("\nList of Running Interfaces:\n")
             for i in range(iterations):
-                print(f"\t\t\t\t{i+1}. {list_of_interfaces_running[i]}")
+                print(f"{i+1}. {list_of_interfaces_running[i]}")
             try:
-                choice = int(input("\n\t\t\t\t -> "))
+                print("\nChoose One of the foillowing Network Interface :")
+                choice = int(input("-> "))
                 
                 if choice > iterations or choice < 1:
                     os.system('clear')
-                    print("\n\t\t\t\tWrong Entry Try Again !!!! ")
+                    print("Wrong Entry Try Again !!!! ")
                 else:
                     break   
             except ValueError:
                 # Handle the case where the input is not an integer
                 os.system('clear')
-                print("\n\t\t\t\tInvalid input. Please enter an integer.")
+                print("Invalid input. Please enter an integer.")
     return list_of_interfaces_running[choice - 1]
 
 
 def get_local_network_info() ->str :
 
-    """Gets Subnet"""
+    """Returns the Local Subnet"""
     interface = network_interface_info()
     local_ip = bash(command=f"ifconfig {interface} | grep -A 1 'inet ' | awk '$1 == \"inet\" {{print $2}}'")
     subnet = '.'.join(local_ip.split('.')[:-1]) + '.0/24'
     return subnet
 
 
+# Clear screen 
+os.system('clear')
+subnet =  get_local_network_info()
 
 os.system('clear')
-get_local_network_info()
+print(f"\nRunning NetDiscover on local subnet : {subnet}\n")
+ips = bash(f"sudo netdiscover -P -r {subnet} | grep '1' | cut -d ' ' -f2 ").splitlines()
+
+for i in range(0,len(ips)):
+    if i == 0 :
+            print(f"{i} - Exit")
+    ip = ips[i]
+    print(f"{i+1} - {ip}")
+
+
