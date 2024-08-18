85cebfe20b856547786ed297f7db7099ab8808fe
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-08-18 12:40:02 -0400

    .

--- network_scanner/__init__.py
+++ network_scanner/__init__.py
@@ -1,1 +1,16 @@
-# Initialize the network_scanner package
+# __init__.py
+
+"""
+Automate Network Scanning Package
+
+This package includes functionality for automating network scans
+using tools like NetDiscover and Nmap. It interacts with system
+utilities to gather network interface information and performs
+scanning tasks based on user inputs.
+"""
+
+# Import key functions from main.py
+from .main import bash, network_interface_info, get_local_network_info, nmap
+
+# Define any package-level constants or settings here if needed
+PACKAGE_NAME = "Automate Network Scanning"
