b8da443f14571819aff7e215372c30e633f85feb
B3TA-BLOCKER <hassaanalibukhari@gmail.com>
2024-08-18 22:11:07 +0500

    .

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add the root directory of the project to the PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from network_scanner.main import network_interface_info, get_local_network_info, nmap
 
 
@@ -14,8 +8,6 @@ def main():
     print("\nLocal Network Info:")
     print(get_local_network_info())
 
-    print("\nRunning Nmap Scan on 192.168.1.1:")
-    nmap("192.168.1.1")
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_import.py
@@ -0,0 +1,3 @@
+from network_scanner.main import network_interface_info, get_local_network_info, nmap
+
+print("Imports successful!")
